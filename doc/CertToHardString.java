ef35c860db627a760575a2fa9a21c5507305c332
Yang Zhou <yangzhou@std.uestc.edu.cn>
2015-12-10 13:55:35 +0800

    修改了证书硬编码的代码

--- doc/CertToHardString.java
+++ doc/CertToHardString.java
@@ -1,9 +1,9 @@
-//package org.esec.mcg.utils;
+// package org.esec.mcg.utils;
 /**
  * java CertToHardString cert.der
  * 将DER编码的二进制证书文件转换成字符串：(byte)0x01, (byte)0x02...
  * 主要是方便将其写死到代码里。。。
- * 需要放在包org.esec.mcg.utils下，将FileUtil.java，ByteUtil.java中的package包申明注释掉
+ * 需要放在包org.esec.mcg.utils下，将FileUtil.java，ByteUtil.java，StringUtil.java中的package包申明注释掉
  * (否则，如果不能单独编译这两个文件，而是需要编译整个包的所有文件)
  * javac FileUtile.java ByteUtil.java
  * javac CertToHardString.java
@@ -13,29 +13,23 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Set;
+import java.util.HashSet;
 
+@SuppressWarnings("unchecked")
 public class CertToHardString {
+
 	public static void main(String[] args) {
 		File file = new File(args[0]);
 		byte[] result = FileUtil.FileToByteArray(file);
 		String s = ByteUtil.ByteArrayToHexString(result);
-		StringBuilder sb = new StringBuilder();
-		sb.append("(byte)");
-		int len = s.length();
-		for (int i = 0; i < len; i++ ) {
-			char c = s.charAt(i);
-			if(c == ' ') {
-				sb.append(", (byte)0x");
-			} else {
-				sb.append(c);
-			}
-		}
+		s = StringUtil.HexStringToHardCodeByteArrayString(s);
 		// System.out.println(sb.toString());
 		FileWriter fw;
 		try {
 			File outFile = new File("out.txt");
 			fw = new FileWriter(outFile);
-			fw.write(sb.toString());
+			fw.write(s);
 			fw.flush();
 		} catch(IOException e) {
 			e.printStackTrace();
