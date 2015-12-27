6a66f07104f5ada0a29030a478fbbd274717a1c0
Yang Zhou <yangzhou@std.uestc.edu.cn>
2015-12-27 13:43:36 +0800

    modify the compile command

--- src/complie-jcardsim-3.0.4.bat
+++ src/complie-jcardsim-3.0.4.bat
@@ -1,2 +1,2 @@
-javac -g -classpath E:\workspace\java\javacard\JCDK3.0.4_ClassicEdition\lib\api_classic.jar;..\extra\jce-jdk13-153.jar com\esec\u2ftoken\*.java
+javac -g -classpath "%JC_CLASSIC_HOME%"\lib\api_classic.jar com\esec\u2ftoken\*.java
 java -cp jcardsim-3.0.4-SNAPSHOT.jar;. com.licel.jcardsim.utils.APDUScriptTool jcardsim.cfg apdu.script output.txt
