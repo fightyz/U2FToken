023f84df584350397c2216ec3a5f6dfab9b03f47
Yang Zhou <yangzhou@std.uestc.edu.cn>
2015-12-27 12:13:21 +0800

    Add the instruction of setting attestation certificate and setting attestation private key

--- src/com/esec/u2ftoken/SecP256r1.java
+++ src/com/esec/u2ftoken/SecP256r1.java
@@ -3,6 +3,7 @@
  */
 package com.esec.u2ftoken;
 
+import javacard.security.ECKey;
 import javacard.security.ECPrivateKey;
 import javacard.security.ECPublicKey;
 import javacard.security.KeyBuilder;
@@ -44,6 +45,8 @@ public class SecP256r1 {
 		0x00, (byte) 0xff, (byte) 0xff, (byte) 0xff, (byte) 0xff, (byte) 0xff, (byte) 0xff, (byte) 0xff,
 		(byte) 0xff, (byte) 0xbc, (byte) 0xe6, (byte) 0xfa, (byte) 0xad, (byte) 0xa7, 0x17, (byte) 0x9e,
 		(byte) 0x84, (byte) 0xf3, (byte) 0xb9, (byte) 0xca, (byte) 0xc2, (byte) 0xfc, 0x63, 0x25, 0x51 };
+	
+	private final static byte k = 0x01;
 
 	public static KeyPair newKeyPair() {
 		KeyPair key = new KeyPair(KeyPair.ALG_EC_FP, (short) 256);
@@ -56,6 +59,7 @@ public class SecP256r1 {
 		privKey.setB(b, (short) 0, (short) b.length);
 		privKey.setG(G, (short) 0, (short) G.length);
 		privKey.setR(r, (short) 0, (short) r.length);
+		privKey.setK((short) 1);
 
 //		byte[] privData = {(byte)0x25, (byte)0xc9, (byte)0xec, (byte)0xdc, (byte)0x4c, (byte)0x59, (byte)0xa3, (byte)0xe0, (byte)0x4f, (byte)0x01, (byte)0x56, (byte)0x97, (byte)0xf3, (byte)0xcb, (byte)0x60, (byte)0x5b, (byte)0x84, (byte)0x49, (byte)0x45, (byte)0x3a, (byte)0xe2, (byte)0x0e, (byte)0xd1, (byte)0xbd, (byte)0xc0, (byte)0xa7, (byte)0xe1, (byte)0xfa, (byte)0x82, (byte)0xee, (byte)0x3c, (byte)0x73};
 //		privKey.setS(privData, (short) 0, (short) privData.length);
@@ -66,11 +70,21 @@ public class SecP256r1 {
 		pubKey.setB(b, (short) 0, (short) b.length);
 		pubKey.setG(G, (short) 0, (short) G.length);
 		pubKey.setR(r, (short) 0, (short) r.length);
+		pubKey.setK((short) 1);
 		
 //		byte[] pubData = {0x04, 0x00, (byte)0xb9, (byte)0x8f, (byte)0xcf, (byte)0xc3, (byte)0xc0, (byte)0xae, (byte)0x95, 0x6a, 0x5b, 0x12, 0x6d, (byte)0xbe, 0x43, (byte)0xe4, 0x7f, 0x09, 0x0d, (byte)0xde, 0x02, (byte)0xd2, 0x6b, 0x28, (byte)0x86, (byte)0xed, 0x2b, (byte)0xd7, (byte)0xe2, (byte)0xc2, 0x69, (byte)0xc1, (byte)0x89, (byte)0xb2, 0x53, (byte)0x96, (byte)0xc1, 0x2d, (byte)0xbf, 0x4c, 0x30, (byte)0xae, (byte)0xd5, (byte)0xd5, 0x3c, (byte)0xb5, (byte)0xf9, 0x3b, 0x20, 0x37, (byte)0x83, (byte)0x88, (byte)0x9f, 0x34, 0x74, (byte)0xf5, 0x6c, (byte)0x97, 0x1e, 0x0a, (byte)0xa9, (byte)0xe7, (byte)0xfa, (byte)0xa6, 0x69};
 //		pubKey.setW(pubData, (short) 0, (short)pubData.length);
 //		pubKey.setK((short) 1);
 		
 		return key;
+	}
+	
+	public static void setCurveParameters(ECKey key) {
+		key.setFieldFP(p, (short) 0, (short) p.length);
+		key.setA(a, (short) 0, (short) a.length);
+		key.setB(b, (short) 0, (short) b.length);
+		key.setG(G, (short) 0, (short) G.length);
+		key.setR(r, (short) 0, (short) r.length);
+		key.setK(k);
 	}
 }
--- src/com/esec/u2ftoken/U2FToken.java
+++ src/com/esec/u2ftoken/U2FToken.java
@@ -19,6 +19,10 @@ import javacard.security.Signature;
 import javacardx.crypto.Cipher;
 
 public class U2FToken extends Applet {
+	
+	private static ECPrivateKey attestationPrivateKey;
+	private static boolean attestationCertificateSet;
+	private static boolean attestationPrivateKeySet;
 	/**
 	 * 64 bytes, contains 32 bytes application sha256 and 32 bytes challenge sha256(this is a hash of Client Data)
 	 */
@@ -34,8 +38,12 @@ public class U2FToken extends Applet {
 	 */
 	private static final short LEN_CHALLENGESHA256 = 32;
 	
-	public static final byte CLA_7816 = 0x00;
+	private static final byte CLA_7816 = 0x00;
 	private static final byte CLA_U2F = 0x00;
+	private static final byte CLA_PROPRIETARY = (byte)0xF0;
+	
+	private static final byte INS_SET_ATTESTATION_CERT = 0x01;
+	private static final byte INS_SET_ATTESTATION_PRIVATE_KEY = 0x02;
 	
 	public static final byte INS_TEST_ENCRYPT = 0x10;
 	public static final byte INS_TEST_DECRYPT = 0x20;
@@ -50,17 +58,14 @@ public class U2FToken extends Applet {
 	public static final byte INS_U2F_CHECK_REGISTER = 0x04; // Registration command that incorporates checking key handles
 	public static final byte INS_U2F_AUTHENTICATE_BATCH = 0x05; // Authenticate/sign command for a batch of key handles
 	
-	/**
-	 * 存储attestation证书的二进制文件。FID是EF01
-	 */
-	public BinaryEF attestationCertFile;
+	private static final short ATTESTATION_SIGNATURE_SIZE = 75;
 	
-	/**
-	 * 版本号："U2F_V2"
-	 */
-	private static final byte[] version = {(byte)0x55, (byte)0x32, (byte)0x46, (byte)0x5F, (byte)0x56, (byte)0x32};
+	private static final byte[] VERSION = {'U', '2', 'F', '_', 'V', '2'};
 	
-	private static VariableLengthRecordEF mAttestationCertificate;
+//	private static final byte[] ATTESTATION_CERTIFICATE = {0x30, (byte)0x82, 0x01, 0x15, 0x30, (byte)0x81, (byte)0xbc, 0x02, 0x09, 0x00, (byte)0xc5, (byte)0xf4, (byte)0xee, 0x4c, 0x59, 0x50, 0x3e, 0x05, 0x30, 0x0a, 0x06, 0x08, 0x2a, (byte)0x86, 0x48, (byte)0xce, 0x3d, 0x04, 0x03, 0x02, 0x30, 0x13, 0x31, 0x11, 0x30, 0x0f, 0x06, 0x03, 0x55, 0x04, 0x03, 0x13, 0x08, 0x59, 0x61, 0x6e, 0x67, 0x5a, 0x68, 0x6f, 0x75, 0x30, 0x1e, 0x17, 0x0d, 0x31, 0x35, 0x31, 0x32, 0x30, 0x39, 0x30, 0x37, 0x30, 0x34, 0x35, 0x38, 0x5a, 0x17, 0x0d, 0x31, 0x36, 0x31, 0x32, 0x30, 0x38, 0x30, 0x37, 0x30, 0x34, 0x35, 0x38, 0x5a, 0x30, 0x13, 0x31, 0x11, 0x30, 0x0f, 0x06, 0x03, 0x55, 0x04, 0x03, 0x13, 0x08, 0x59, 0x61, 0x6e, 0x67, 0x5a, 0x68, 0x6f, 0x75, 0x30, 0x59, 0x30, 0x13, 0x06, 0x07, 0x2a, (byte)0x86, 0x48, (byte)0xce, 0x3d, 0x02, 0x01, 0x06, 0x08, 0x2a, (byte)0x86, 0x48, (byte)0xce, 0x3d, 0x03, 0x01, 0x07, 0x03, 0x42, 0x00, 0x04, 0x72, (byte)0x9a, 0x71, (byte)0xd0, (byte)0x81, 0x62, 0x42, (byte)0x84, (byte)0x92, (byte)0xf2, (byte)0xd9, 0x61, (byte)0x92, 0x4d, 0x37, 0x44, 0x3a, 0x4f, 0x1b, (byte)0xda, 0x58, 0x0f, (byte)0x8a, (byte)0xea, 0x29, 0x20, (byte)0xd2, (byte)0x99, 0x7c, (byte)0xbe, (byte)0xa4, 0x39, 0x60, (byte)0xce, 0x72, (byte)0x9e, 0x35, (byte)0xc1, (byte)0xf7, 0x40, (byte)0x92, (byte)0xf2, 0x25, 0x0e, 0x60, 0x74, (byte)0x82, 0x3f, (byte)0xc5, 0x7f, 0x33, 0x60, (byte)0xb7, (byte)0xcd, 0x39, 0x69, (byte)0xc3, (byte)0xc3, 0x12, 0x5e, (byte)0xce, 0x26, 0x5c, 0x29, 0x30, 0x0a, 0x06, 0x08, 0x2a, (byte)0x86, 0x48, (byte)0xce, 0x3d, 0x04, 0x03, 0x02, 0x03, 0x48, 0x00, 0x30, 0x45, 0x02, 0x21, 0x00, (byte)0xe7, 0x67, (byte)0xfa, (byte)0x94, 0x10, 0x35, (byte)0xd5, (byte)0x85, 0x3d, 0x52, (byte)0xd8, 0x7d, 0x67, 0x14, 0x70, (byte)0xbc, 0x76, 0x3b, (byte)0xc5, (byte)0xb1, 0x2e, 0x1d, 0x45, 0x77, (byte)0xea, (byte)0x9f, (byte)0x8c, (byte)0xa6, 0x74, (byte)0xe5, (byte)0x9d, 0x39, 0x02, 0x20, 0x3f, (byte)0xe1, 0x1c, (byte)0xad, 0x59, (byte)0xf5, 0x35, 0x76, 0x00, 0x1f, 0x15, (byte)0xee, 0x05, (byte)0xda, (byte)0x87, 0x46, (byte)0xfe, (byte)0xd3, 0x27, 0x6b, 0x16, (byte)0x82, (byte)0x9e, (byte)0x9d, 0x5e, (byte)0xfd, (byte)0xff, 0x70, 0x5e, 0x08, (byte)0x9c, 0x6d};
+//	private static final byte[] ATTESTATION_PRIVATE_KEY = {0x4c, (byte)0xc7, (byte)0xcf, 0x68, (byte)0x91, 0x18, (byte)0x96, (byte)0xc8, (byte)0xe2, (byte)0xf9, (byte)0xc8, (byte)0xcc, 0x2f, 0x7f, 0x0a, (byte)0xa2, 0x1c, 0x6a, (byte)0xcb, (byte)0xba, 0x38, 0x1c, 0x10, (byte)0x9a, (byte)0xfe,(byte)0x91, 0x18, (byte)0xf6, (byte)0xca, (byte)0xd9, 0x0f, 0x0b};
+	private static byte[] ATTESTATION_CERTIFICATE;
+	private static byte[] ATTESTATION_PRIVATE_KEY;
 	
 	public SecretKeys mSecretKey;
 	private AESKey mAESKeyInstance;
@@ -69,12 +74,24 @@ public class U2FToken extends Applet {
 	private ECPublicKey pubKey;
 	private ECPrivateKey privKey;
 	private boolean GENED = false;
-	private Signature signature;
+	private static Signature signature;
+	
+	private static Signature attestationSignature;
+	private static Signature authenticateSignature;
+	/**
+	 * To store the attestation signature so that it can be handled by GetData
+	 */
+	private static byte[] signatureMessage;
 	
 	private KeyHandleGenerator mKeyHandleGenerator;
 	
 	public U2FToken() {
+		signatureMessage = JCSystem.makeTransientByteArray(ATTESTATION_SIGNATURE_SIZE, JCSystem.CLEAR_ON_DESELECT);
+		
 		mKeyHandleGenerator = new IndexKeyHandle();
+		
+		attestationSignature = Signature.getInstance(Signature.ALG_ECDSA_SHA, false);
+		authenticateSignature = Signature.getInstance(Signature.ALG_ECDSA_SHA, false);
 	}
 	public static void install(byte[] bArray, short bOffset, byte bLength) {
 		// GP-compliant JavaCard applet registration
@@ -95,48 +112,69 @@ public class U2FToken extends Applet {
 		byte p2 = buf[ISO7816.OFFSET_P2];
 		short lc = (short)(buf[ISO7816.OFFSET_LC] & 0x00FF);
 		
-		switch (buf[ISO7816.OFFSET_INS]) {
-		case (byte) INS_TEST_ENCRYPT:
-//			try {
-//				KeyBuilder.buildKey(KeyBuilder.TYPE_AES, KeyBuilder.LENGTH_AES_128, false);
-//			} catch(CryptoException e) {
-//				short reason = e.getReason();
-////				ISOException.throwIt(JCSystem.getVersion());
-////				ISOException.throwIt(reason);
-//			}
-			try {
-				Cipher.getInstance(Cipher.ALG_AES_BLOCK_128_CBC_NOPAD, false);
-			} catch(CryptoException e) {
-				ISOException.throwIt(JCSystem.getVersion());
-				short reason = e.getReason();
-				ISOException.throwIt(reason);
+		if (cla == CLA_PROPRIETARY) {
+			if (attestationCertificateSet && attestationPrivateKeySet) {
+				ISOException.throwIt(ISO7816.SW_SECURITY_STATUS_NOT_SATISFIED);
 			}
-			AESencrypt(apdu, cla, p1, p2, lc);
-			break;
-		case (byte) INS_TEST_DECRYPT:
-//			decrypt(apdu, cla, p1, p2, lc);
-			break;
-		case (byte) INS_U2F_REGISTER: // U2F register command
-			u2fRegister(apdu, cla, p1, p2, lc);
-			break;
+			switch (buf[ISO7816.OFFSET_INS]) {
+			case INS_SET_ATTESTATION_CERT:
+				setAttestationCert(apdu, cla, p1, p2, lc);
+				break;
+			case INS_SET_ATTESTATION_PRIVATE_KEY:
+				setAttestationPrivateKey(apdu, cla, p1, p2, lc);
+				break;
+			default:
+				ISOException.throwIt(ISO7816.SW_INS_NOT_SUPPORTED);
+			}
+		} else if (cla == CLA_U2F) {
+			if (!attestationCertificateSet || !attestationPrivateKeySet) {
+				ISOException.throwIt(ISO7816.SW_SECURITY_STATUS_NOT_SATISFIED);
+			}
+			switch (buf[ISO7816.OFFSET_INS]) {
+			case (byte) INS_TEST_ENCRYPT:
+//				try {
+//					KeyBuilder.buildKey(KeyBuilder.TYPE_AES, KeyBuilder.LENGTH_AES_128, false);
+//				} catch(CryptoException e) {
+//					short reason = e.getReason();
+////					ISOException.throwIt(JCSystem.getVersion());
+////					ISOException.throwIt(reason);
+//				}
+				try {
+					Cipher.getInstance(Cipher.ALG_AES_BLOCK_128_CBC_NOPAD, false);
+				} catch(CryptoException e) {
+					ISOException.throwIt(JCSystem.getVersion());
+					short reason = e.getReason();
+					ISOException.throwIt(reason);
+				}
+				AESencrypt(apdu, cla, p1, p2, lc);
+				break;
+			case (byte) INS_TEST_DECRYPT:
+//				decrypt(apdu, cla, p1, p2, lc);
+				break;
+			case (byte) INS_U2F_REGISTER: // U2F register command
+				u2fRegister(apdu, cla, p1, p2, lc);
+				break;
+				
+			case (byte) INS_TEST_SEEECPUBKEY:
+				seeECPubKey(apdu, cla, p1, p2, lc);
+				break;
 			
-		case (byte) INS_TEST_SEEECPUBKEY:
-			seeECPubKey(apdu, cla, p1, p2, lc);
-			break;
-		
-		case (byte) INS_TEST_VERIFY:
-			verifyKey(apdu, cla, p1, p2, lc);
-			break;
-			
-		case (byte) INS_TEST_GENERIC:
-			byte[] test = genericTest(apdu, cla, p1, p2, lc);
-			Util.arrayCopyNonAtomic(test, (short) 0, buf, (short) 0, (short) test.length);
-			apdu.setOutgoingAndSend((short) 0, (short) test.length);
-			break;
-			
-		default:
-			// good practice: If you don't know the INStruction, say so:
-			ISOException.throwIt(ISO7816.SW_INS_NOT_SUPPORTED);
+			case (byte) INS_TEST_VERIFY:
+				verifyKey(apdu, cla, p1, p2, lc);
+				break;
+				
+			case (byte) INS_TEST_GENERIC:
+				byte[] test = genericTest(apdu, cla, p1, p2, lc);
+				Util.arrayCopyNonAtomic(test, (short) 0, buf, (short) 0, (short) test.length);
+				apdu.setOutgoingAndSend((short) 0, (short) test.length);
+				break;
+				
+			default:
+				// good practice: If you don't know the INStruction, say so:
+				ISOException.throwIt(ISO7816.SW_INS_NOT_SUPPORTED);
+			}
+		} else {
+			ISOException.throwIt(ISO7816.SW_CLA_NOT_SUPPORTED);
 		}
 	}
 	
@@ -146,8 +184,28 @@ public class U2FToken extends Applet {
 	 */
 	private void getSelectResponse(APDU apdu) {
 		byte[] buffer = apdu.getBuffer();
-		Util.arrayCopyNonAtomic(version, (short)0, buffer, (short)0, (short)version.length);
-		apdu.setOutgoingAndSend((short)0, (short)version.length);
+		Util.arrayCopyNonAtomic(VERSION, (short)0, buffer, (short)0, (short)VERSION.length);
+		apdu.setOutgoingAndSend((short)0, (short)VERSION.length);
+	}
+	
+	private void setAttestationCert(APDU apdu, byte cla, byte p1, byte p2, short lc) {
+		short len = apdu.setIncomingAndReceive();
+		byte[] buffer = apdu.getBuffer();
+		ATTESTATION_CERTIFICATE = new byte[len];
+		Util.arrayCopy(buffer, ISO7816.OFFSET_CDATA, ATTESTATION_CERTIFICATE, (short) 0, len);
+		attestationCertificateSet = true;
+	}
+	
+	private void setAttestationPrivateKey(APDU apdu, byte cla, byte p1, byte p2, short lc) {
+		short len = apdu.setIncomingAndReceive();
+		byte[] buffer = apdu.getBuffer();
+		ATTESTATION_PRIVATE_KEY = new byte[len];
+		Util.arrayCopy(buffer, ISO7816.OFFSET_CDATA, ATTESTATION_PRIVATE_KEY, (short) 0, len);
+		attestationPrivateKey = (ECPrivateKey) KeyBuilder.buildkey(KeyBuilder.TYPE_EC_FP_PRIVATE, KeyBuilder.LENGTH_EC_FP_256, false);
+		SecP256r1.setCurveParameters(attestationPrivateKey);
+		attestationPrivateKey.setS(ATTESTATION_PRIVATE_KEY, (short) 0, (short) ATTESTATION_PRIVATE_KEY.length);
+		attestationSignature.init(attestationPrivateKey, Signature.MODE_SIGN);
+		attestationPrivateKeySet = true;
 	}
 
 	/**
@@ -159,9 +217,6 @@ public class U2FToken extends Applet {
 	 * @param lc
 	 */
 	private void u2fRegister(APDU apdu, byte cla, byte p1, byte p2, short lc) {
-		if (cla != CLA_U2F) {
-			ISOException.throwIt(ISO7816.SW_CLA_NOT_SUPPORTED);
-		}
 		short readCount = apdu.setIncomingAndReceive();		
 		if (readCount != LEN_REGISTRATION_REQUEST_MESSAGE) {
 			ISOException.throwIt(ISO7816.SW_WRONG_DATA);
@@ -185,9 +240,6 @@ public class U2FToken extends Applet {
 		
 		// Store user's private key locally. Generate Key Handle.
 		byte[] keyHandle = mKeyHandleGenerator.generateKeyHandle(applicationSha256, privKey);
-		{
-			// TODO May be store tuple(KeyHandle, KeyPair) in two Linear EF. They can be mapped by the index.
-		}
 		
 		short userPublicKeyLen = pubKey.getW(buffer, (short) 0);
 		byte[] userPublicKey = sharedMemory.m65BytesUserPublicKey;
@@ -202,9 +254,7 @@ public class U2FToken extends Applet {
 				);
 		
 		// Generate signature use attestation private key
-		Signature signature = Signature.getInstance(Signature.ALG_ECDSA_SHA, false);
-		signature.init(privKey, Signature.MODE_SIGN);
-		short signLen = signature.sign(signedData, (short) 0, (short) signedData.length, buffer, (short) 0);
+		short signLen = attestationSignature.sign(signedData, (short) 0, (short) signedData.length, signatureMessage, (short) 0);
 		
 		//生成认证公私钥
 //		KeyPair pair = SecP256r1.newKeyPair();
@@ -349,8 +399,8 @@ public class U2FToken extends Applet {
 		
 		try {
 			// Cipher.getInstance在这里过不了，在U2FToken里能过？？？
-//			cipher = Cipher.getInstance(Cipher.ALG_AES_BLOCK_128_ECB_NOPAD, false);
-			cipher = Cipher.getInstance(Cipher.ALG_AES_CBC_ISO9797_M2, false);
+			cipher = Cipher.getInstance(Cipher.ALG_AES_BLOCK_128_ECB_NOPAD, false);
+			// cipher = Cipher.getInstance(Cipher.ALG_AES_CBC_ISO9797_M2, false);
 		} catch (CryptoException e) {
 //			ISOException.throwIt(JCSystem.getVersion());
 			short reason = e.getReason();
