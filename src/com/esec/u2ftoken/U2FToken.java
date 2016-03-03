9b37485b6e3c01484117e1d8297a62279a6533f6
Yang Zhou <yangzhou@std.uestc.edu.cn>
2016-03-03 15:11:38 +0800

    delete some redundant

--- src/com/esec/u2ftoken/U2FToken.java
+++ src/com/esec/u2ftoken/U2FToken.java
@@ -22,12 +22,6 @@ import javacardx.apdu.ExtendedLength;
 
 public class U2FToken extends Applet implements ExtendedLength {
 	
-	/**
-	 * As there's some bugs with ISOException.SW_BYTES_REMAINING_00, I define this
-	 * constant parameter to substitute the ISOException.SW_BYTES_REMAINING_00.
-	 */
-	private static final short SW_BYTES_REMAINING = 0x1100;
-	
 	private static final byte RFU_ENROLL_SIGNED_VERSION[] = { (byte)0x00 };
 	
 	private static ECPrivateKey attestationPrivateKey;
@@ -59,8 +53,6 @@ public class U2FToken extends Applet implements ExtendedLength {
 	 */
 	private static final short LEN_CHALLENGESHA256 = 32;
 	
-	private static final byte CLA_7816 = 0x00;
-	
 	/**
 	 * 0x00
 	 */
@@ -86,13 +78,6 @@ public class U2FToken extends Applet implements ExtendedLength {
 	 */
 	private static final byte INS_SET_ATTESTATION_PRIVATE_KEY = 0x02;
 	
-	public static final byte INS_TEST_ENCRYPT = 0x10;
-	public static final byte INS_TEST_DECRYPT = 0x20;
-	public static final byte INS_TEST_SEEECPUBKEY = 0x30;
-	public static final byte INS_TEST_VERIFY = 0x40;
-	public static final byte INS_TEST_ECDSA = 0x50;
-	public static final byte INS_TEST_GENERIC = 0x60;
-	
 	private static final byte INS_U2F_REGISTER = 0x01; // Registration command
 	private static final byte INS_U2F_AUTHENTICATE = 0x02; // Authenticate/sign command
 	private static final byte INS_U2F_VERSION = 0x03; //Read version string command
@@ -102,21 +87,9 @@ public class U2FToken extends Applet implements ExtendedLength {
 	public static final short U2F_SW_TEST_OF_PRESENCE_REQUIRED = ISO7816.SW_CONDITIONS_NOT_SATISFIED;
 	public static final short U2F_SW_INVALID_KEY_HANDLE = ISO7816.SW_WRONG_DATA;
 	
-	/**
-	 * 75 bytes. The max signature size, maybe some bytes remaining.
-	 */
-	private static final short ATTESTATION_SIGNATURE_SIZE = 75;
-	
 	private static final byte[] VERSION = {'U', '2', 'F', '_', 'V', '2'};
 	
 	private static byte[] ATTESTATION_CERTIFICATE;
-	
-	public SecretKeys mSecretKey;
-	private AESKey mAESKeyInstance;
-	
-	private KeyPair pair;
-	private boolean GENED = false;
-	private static Signature signature;
 	
 	private static Signature attestationSignature;
 	private static Signature authenticateSignature;
@@ -141,6 +114,7 @@ public class U2FToken extends Applet implements ExtendedLength {
 		SecP256r1.keyPair = new KeyPair(KeyPair.ALG_EC_FP, (short) 256);
 		SecP256r1.setCurveParameters((ECKey)SecP256r1.keyPair.getPrivate());
 		SecP256r1.setCurveParameters((ECKey)SecP256r1.keyPair.getPublic());
+		// We safely assume that register response is no more than 1024 bytes.
 		registerResponse = new byte[1024];
 	}
 	public static void install(byte[] bArray, short bOffset, byte bLength) {
@@ -151,7 +125,6 @@ public class U2FToken extends Applet implements ExtendedLength {
 	public void process(APDU apdu) {
 		// Good practice: Return 9000 on SELECT
 		if (selectingApplet()) {
-			registerResponseRemaining = 0;
 			getSelectResponse(apdu);
 			return;
 		}
@@ -182,26 +155,6 @@ public class U2FToken extends Applet implements ExtendedLength {
 				ISOException.throwIt(ISO7816.SW_SECURITY_STATUS_NOT_SATISFIED);
 			}
 			switch (buf[ISO7816.OFFSET_INS]) {
-			case (byte) INS_TEST_ENCRYPT:
-//				try {
-//					KeyBuilder.buildKey(KeyBuilder.TYPE_AES, KeyBuilder.LENGTH_AES_128, false);
-//				} catch(CryptoException e) {
-//					short reason = e.getReason();
-////					ISOException.throwIt(JCSystem.getVersion());
-////					ISOException.throwIt(reason);
-//				}
-				try {
-					Cipher.getInstance(Cipher.ALG_AES_BLOCK_128_CBC_NOPAD, false);
-				} catch(CryptoException e) {
-					ISOException.throwIt(JCSystem.getVersion());
-					short reason = e.getReason();
-					ISOException.throwIt(reason);
-				}
-				AESencrypt(apdu, cla, p1, p2, lc);
-				break;
-			case (byte) INS_TEST_DECRYPT:
-//				decrypt(apdu, cla, p1, p2, lc);
-				break;
 			case (byte) INS_U2F_REGISTER: // U2F register command
 				u2fRegister(apdu, cla, p1, p2, lc);
 				break;
@@ -212,25 +165,6 @@ public class U2FToken extends Applet implements ExtendedLength {
 			
 			case (byte) INS_ISO_GET_DATA:
 				getData(apdu, cla, p1, p2, lc);
-				break;
-				
-			case (byte) INS_TEST_SEEECPUBKEY:
-				seeECPubKey(apdu, cla, p1, p2, lc);
-				break;
-			
-			case (byte) INS_TEST_VERIFY:
-				verifyKey(apdu, cla, p1, p2, lc);
-				break;
-				
-			case (byte) INS_TEST_ECDSA:
-				testECDSA(apdu, cla, p1, p2, lc);
-				ISOException.throwIt(JCSystem.getVersion());
-				break;
-				
-			case (byte) INS_TEST_GENERIC:
-				byte[] test = genericTest(apdu, cla, p1, p2, lc);
-				Util.arrayCopyNonAtomic(test, (short) 0, buf, (short) 0, (short) test.length);
-				apdu.setOutgoingAndSend((short) 0, (short) test.length);
 				break;
 				
 			default:
@@ -263,7 +197,6 @@ public class U2FToken extends Applet implements ExtendedLength {
 	private void setAttestationPrivateKey(APDU apdu, byte cla, byte p1, byte p2, short lc) {
 		short len = apdu.setIncomingAndReceive();
 		byte[] buffer = apdu.getBuffer();
-//		attestationPrivateKey = (ECPrivateKey)SecP256r1.newKeyPair().getPrivate();
 		attestationPrivateKey = (ECPrivateKey)KeyBuilder.buildKey(KeyBuilder.TYPE_EC_FP_PRIVATE, KeyBuilder.LENGTH_EC_FP_256, false);
 		SecP256r1.setCurveParameters(attestationPrivateKey);
 		attestationPrivateKey.setS(buffer, ISO7816.OFFSET_CDATA, len);
@@ -318,7 +251,6 @@ public class U2FToken extends Applet implements ExtendedLength {
 		// Because every time do the signature has a different length, so signatureMessage's first 2 bytes indicate the length.
 		Util.setShort(signatureMessage, (short) 0, signLen);
 		
-		// TODO handle the extended length case.
 		// Generate register response
 		if (extendedLength) {
 			short sendLen = RawMessageCodec.encodeRegisterResponse(userPublicKey, keyHandle, ATTESTATION_CERTIFICATE, signatureMessage, buffer, (short)0);
@@ -327,14 +259,14 @@ public class U2FToken extends Applet implements ExtendedLength {
 			short blockSize = apdu.setOutgoing();
 			short registerResponseLen = RawMessageCodec.encodeRegisterResponse(userPublicKey, keyHandle, ATTESTATION_CERTIFICATE, signatureMessage, registerResponse, (short)2);
 			registerResponseLen -= 2;
-			// Set the register response's sent offset is now 258, as sent 256 data and 2 header bytes(store the offset).
+			// Set the register response's sent offset is now (blockSize+2), as sent blockSize bytes data and 2 header bytes(store the offset).
 			Util.setShort(registerResponse, (short)0, (short)(blockSize + 2));
 			Util.arrayCopyNonAtomic(registerResponse, (short)2, buffer, (short) 0, blockSize);
 			apdu.setOutgoingLength(blockSize);
 			apdu.sendBytes((short)0, blockSize);
 
 			registerResponseRemaining = (short)(registerResponseLen - blockSize);
-			if (registerResponseRemaining > blockSize) {
+			if (registerResponseRemaining > 256) {
 				ISOException.throwIt(ISO7816.SW_BYTES_REMAINING_00);
 			} else if (registerResponseRemaining > 0) {
 				ISOException.throwIt((short)(ISO7816.SW_BYTES_REMAINING_00 + registerResponseRemaining));
@@ -355,7 +287,7 @@ public class U2FToken extends Applet implements ExtendedLength {
 			registerResponseRemaining -= blockSize;
 			apdu.setOutgoingLength(blockSize);
 			apdu.sendBytes((short)0, blockSize);
-			short remainingLen = registerResponseRemaining > blockSize ? ISO7816.SW_BYTES_REMAINING_00 : (short)(ISO7816.SW_BYTES_REMAINING_00 + registerResponseRemaining);
+			short remainingLen = registerResponseRemaining > 256 ? ISO7816.SW_BYTES_REMAINING_00 : (short)(ISO7816.SW_BYTES_REMAINING_00 + registerResponseRemaining);
 			ISOException.throwIt(remainingLen);
 			
 		} else if (registerResponseRemaining > 0) {
@@ -439,150 +371,5 @@ public class U2FToken extends Applet implements ExtendedLength {
         authenticateSignature.init(privKey, Signature.MODE_SIGN);
         outOffset += authenticateSignature.sign(signedData, (short) 0, (short) 69, buffer, outOffset);
         apdu.setOutgoingAndSend((short) 0, outOffset);
-	}
-	
-	private void seeECPubKey(APDU apdu, byte cla, byte p1, byte p2, short lc) {
-		byte[] buffer = apdu.getBuffer();
-		if (cla != CLA_7816) {
-			ISOException.throwIt(ISO7816.SW_CLA_NOT_SUPPORTED);
-		}
-		
-		//生成认证公私钥
-//		KeyPair pair = SecP256r1.newKeyPair();
-//		pair.genKeyPair();
-//		ECPublicKey pubKey = (ECPublicKey) pair.getPublic();
-//		ECPrivateKey privKey = (ECPrivateKey) pair.getPrivate();
-//		// 生成KeyHandle
-//		//TODO 生成KeyHandle，里面的AppID似乎只能是Client传过来的AppID的hash？
-//		
-////		short sendlen = pubKey.getW(buffer, (short) 0);
-//		short sendlen = privKey.getS(buffer, (short) 0);
-		
-//		if (GENED == false) {
-//			pair = SecP256r1.newKeyPair();
-//			pair.genKeyPair();
-//			GENED = true;
-//		}
-//		ECPublicKey pubKey = (ECPublicKey) pair.getPublic();
-//		ECPrivateKey privKey = (ECPrivateKey) pair.getPrivate();
-//		// 生成KeyHandle
-//		//TODO 生成KeyHandle，里面的AppID似乎只能是Client传过来的AppID的hash？
-//		
-////		short sendlen = pubKey.getW(buffer, (short) 0);
-//		short sendlen = pubKey.getW(buffer, (short) 0);
-//		privKey.get
-//		pubKey.
-		
-//		apdu.setOutgoingAndSend((short) 0, sendlen);
-	}
-	
-	public void verifyKey(APDU apdu, byte cla, byte p1, byte p2, short lc) {
-		byte[] buffer = apdu.getBuffer();
-		if (cla != CLA_7816) {
-			ISOException.throwIt(ISO7816.SW_CLA_NOT_SUPPORTED);
-		}
-		
-		if (GENED == false) {
-			pair = SecP256r1.newKeyPair();
-			try {
-				pair.genKeyPair();
-			} catch(CryptoException e) {
-				ISOException.throwIt(JCSystem.getVersion());
-			}
-			
-			GENED = true;
-		}
-		ECPublicKey pubKey = (ECPublicKey) pair.getPublic();
-		ECPrivateKey privKey = (ECPrivateKey) pair.getPrivate();
-//		byte[] pubData = {0x04, 0x00, (byte)0xb9, (byte)0x8f, (byte)0xcf, (byte)0xc3, (byte)0xc0, (byte)0xae, (byte)0x95, 0x6a, 0x5b, 0x12, 0x6d, (byte)0xbe, 0x43, (byte)0xe4, 0x7f, 0x09, 0x0d, (byte)0xde, 0x02, (byte)0xd2, 0x6b, 0x28, (byte)0x86, (byte)0xed, 0x2b, (byte)0xd7, (byte)0xe2, (byte)0xc2, 0x69, (byte)0xc1, (byte)0x89, (byte)0xb2, 0x53, (byte)0x96, (byte)0xc1, 0x2d, (byte)0xbf, 0x4c, 0x30, (byte)0xae, (byte)0xd5, (byte)0xd5, 0x3c, (byte)0xb5, (byte)0xf9, 0x3b, 0x20, 0x37, (byte)0x83, (byte)0x88, (byte)0x9f, 0x34, 0x74, (byte)0xf5, 0x6c, (byte)0x97, 0x1e, 0x0a, (byte)0xa9, (byte)0xe7, (byte)0xfa, (byte)0xa6, 0x69};
-//		pubKey.setW(pubData, (short) 0, (short)pubData.length);
-//		byte[] privData = {(byte)0x25, (byte)0xc9, (byte)0xec, (byte)0xdc, (byte)0x4c, (byte)0x59, (byte)0xa3, (byte)0xe0, (byte)0x4f, (byte)0x01, (byte)0x56, (byte)0x97, (byte)0xf3, (byte)0xcb, (byte)0x60, (byte)0x5b, (byte)0x84, (byte)0x49, (byte)0x45, (byte)0x3a, (byte)0xe2, (byte)0x0e, (byte)0xd1, (byte)0xbd, (byte)0xc0, (byte)0xa7, (byte)0xe1, (byte)0xfa, (byte)0x82, (byte)0xee, (byte)0x3c, (byte)0x73};
-//		privKey.setS(privData, (short) 0, (short) privData.length);
-		
-		byte[] data = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f};
-		
-		
-		signature = Signature.getInstance(Signature.ALG_ECDSA_SHA_256, false);
-		
-		try {
-			signature.init(pair.getPrivate(), Signature.MODE_SIGN);
-		} catch(CryptoException e) {
-			ISOException.throwIt(JCSystem.getVersion());
-		}
-		byte[] signData = new byte[127];
-		short signLen = signature.sign(data, (short) 0, (short) data.length, signData, (short) 0);
-//		short sendLen = signature.sign(data, (short) 0, (short) data.length, buffer, (short) 0);
-//		apdu.setOutgoingAndSend((short) 0, sendLen);
-		
-		byte[] certData = new byte[(short)(16 + signLen)];
-		Util.arrayCopyNonAtomic(data, (short) 0, certData, (short) 0, (short) data.length);
-		Util.arrayCopyNonAtomic(signData, (short) 0, certData, (short) data.length, signLen);
-		signature.init(pair.getPublic(), Signature.MODE_VERIFY);
-		if (signature.verify(certData, (short) 0, (short) data.length, signData, (short) 0, signLen)) {
-			ISOException.throwIt((short)0x0001);
-		} else {
-			ISOException.throwIt((short)0x0004);
-		}
-		apdu.setOutgoingAndSend((short) 0, (short) data.length);
-	}
-	
-	public void encrypt(APDU apdu, byte cla, byte p1, byte p2, short lc) {
-		byte[] buffer = apdu.getBuffer();
-		mSecretKey = new SecretKeys(SecretKeys.KEY_TYPE_AES);
-		byte[] data = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f};
-		mSecretKey.keyWrap(data, (short) 0, (short) data.length, buffer, (short) 0, SecretKeys.MODE_ENCRYPT);
-//		byte[] data = {0x49, 0x1E, (byte) 0x89, 0x0D, (byte) 0xE9, (byte) 0xAC, (byte) 0xE9, 0x32, (byte) 0x83, (byte) 0x8A, 0x49, 0x79, 0x2F, 0x22, 0x13, (byte) 0xF3};
-//		secretKey.keyWrap(data, (short) 0, (short) 16, buffer, (short) 0, SecretKey.MODE_DECRYPT);
-		apdu.setOutgoingAndSend((short) 0, (short) 48);
-	}
-	
-	public void AESencrypt(APDU apdu, byte cla, byte p1, byte p2, short lc) {
-		byte[] buffer = apdu.getBuffer();
-		try {
-			// TODO 这里有点问题，没有这个算法？
-			mAESKeyInstance = (AESKey) KeyBuilder.buildKey(KeyBuilder.TYPE_AES, KeyBuilder.LENGTH_AES_128, false);
-		} catch(CryptoException e) {
-			short reason = e.getReason();
-			ISOException.throwIt(reason);
-		}
-		byte[] keyData = JCSystem.makeTransientByteArray((short) 16, JCSystem.CLEAR_ON_DESELECT);
-		Util.arrayFillNonAtomic(keyData, (short) 0, (short) keyData.length, (byte) 0x00);
-		mAESKeyInstance.setKey(keyData, (short) 0);
-		
-		
-		byte[] data = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f};
-		Cipher cipher = null;
-		
-		try {
-			// Cipher.getInstance在这里过不了，在U2FToken里能过？？？
-			cipher = Cipher.getInstance(Cipher.ALG_AES_BLOCK_128_ECB_NOPAD, false);
-			// cipher = Cipher.getInstance(Cipher.ALG_AES_CBC_ISO9797_M2, false);
-		} catch (CryptoException e) {
-//			ISOException.throwIt(JCSystem.getVersion());
-			short reason = e.getReason();
-			ISOException.throwIt(reason);
-		}
-		cipher.init(mAESKeyInstance, Cipher.MODE_ENCRYPT); // 初始向量(iv)是0
-//		}
-		
-		// 加密或解密，doFinal后，cipher对象将被重置
-		short sendLen = 0;
-		try {
-			sendLen = cipher.doFinal(data, (short) 0, (short) data.length, buffer, (short) 0);
-		} catch(Exception e) {
-			ISOException.throwIt(ISO7816.SW_WRONG_DATA);
-		}
-		apdu.setOutgoingAndSend((short) 0, sendLen);
-	}
-	
-	private byte[] genericTest(APDU apdu, byte cla, byte p1, byte p2, short lc) {
-		ISOException.throwIt(apdu.setOutgoing());
-		return null;
-	}
-	
-	private byte[] testECDSA(APDU apdu, byte cla, byte p1, byte p2, short lc) {
-		
-		
-		return null;
 	}
 }
